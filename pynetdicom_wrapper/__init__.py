740d13f3d768d55aacb943ab351288efbe7ea94e
Thomas Bezold <thomas.bezold@unimedizin-mainz.de>
2024-10-16 09:09:08 +0200

    changed __init__.py for new setup

--- pynetdicom_wrapper/__init__.py
+++ pynetdicom_wrapper/__init__.py
@@ -1,1 +1,7 @@
-from .pynetdicom_wrapper import PynetdicomWrapper
+"""pynetdicom_wrapper provides functions to use pynetdicom
+with Aria more easily.
+"""
+import pynetdicom_wrapper.datasets
+from pynetdicom_wrapper.wrapper import PynetdicomWrapper
+
+__version__ = "0.0.2"
